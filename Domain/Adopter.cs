b2558d03f161063e05151f9a6dc59fc3da73234a
mare <markoilioski21@gmail.com>
2024-12-18 22:33:14 +0100

    Added the file now waiting for Adoption and the other one.

---
+++ Domain/Adopter.cs
@@ -0,0 +1,18 @@
+namespace Domain;
+
+public class Adopter
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public bool HasChildren { get; set; }
+    public bool HasOtherPets { get; set; }
+    public string HomeType { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<AdopterPetTypePreference> AdopterPetTypePreferences { get; set; }
+    public virtual ICollection<Adoption> Adoptions { get; set; }
+}
