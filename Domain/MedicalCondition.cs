50cdbb227a4e7ccbe0089ca5e866ae89515e7507
Sara Apostolovska <sara.apostolovska@students.finki.ukim.mk>
2024-12-18 21:19:54 +0100

    Added required foreign key property.

--- Domain/MedicalCondition.cs
+++ Domain/MedicalCondition.cs
@@ -5,16 +5,19 @@ namespace Domain;
 [Table("medical_conditions")]
 public class MedicalCondition : BaseEntity
 {
+    [Required]
     [ForeignKey(nameof(MedicalRecord))]
-    [Column("medical_record_id", TypeName = "UUID")]
+    [Column("medical_record_id", TypeName = "UUID")] 
     public Guid MedicalRecordId { get; set; }
-
+    
+    [Required]
     [Column("condition_name", TypeName = "VARCHAR(100")]
     public string ConditionName { get; set; }
 
     [Column("notes", TypeName = "VARCHAR(255)")]
     public string? Notes { get; set; }
 
+    public Guid MedicalRecordId { get; set; } // Required foreign key property
     public MedicalRecord MedicalRecord { get; init; } = null!;
 
 }
