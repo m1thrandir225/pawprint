210644594816794c5b5d102dab8871b3d148163d
mare <markoilioski21@gmail.com>
2024-12-18 22:38:04 +0100

    Added Adoption.cs but need Pet and Adopter to be implemented.

---
+++ Domain/Adoption.cs
@@ -0,0 +1,18 @@
+namespace Domain;
+
+public class Adoption
+{
+    // Foreign key notations is not required if it's named like this.
+    public Guid Id { get; set; }
+    public Guid PetId { get; set; }
+    public Guid AdopterId { get; set; }
+    public DateTime AdoptionDate { get; set; }
+    public decimal AdoptionFee { get; set; }
+    public DateTime? FollowUpDate { get; set; }
+    public string CounselorNotes { get; set; }
+    public bool IsSuccessfull { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public virtual Pet Pet { get; set; }
+    public virtual Adopter Adopter { get; set; }
+}
