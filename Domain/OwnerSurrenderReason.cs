19dca6be22f83c30626c56b45c8c06595151768a
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-19 19:24:47 +0100

    feat: renamed table after db changes

---
+++ Domain/OwnerSurrenderReason.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Domain;
+
+public class OwnerSurrenderReason : BaseEntity
+{
+    [Required]
+    [Column("description", TypeName = "VARCHAR(255)")]
+    public string Description { get; set; }
+}
