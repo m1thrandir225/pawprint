141e27021b33c8f3308a35fbfb9315068b9cc3b9
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-19 19:34:03 +0100

    fix: fixed double class definition

--- Domain/AdopterPetTypePreference.cs
+++ Domain/AdopterPetTypePreference.cs
@@ -6,19 +6,15 @@ namespace Domain;
 [Table("adopter_pet_type_preferences")]
 public class AdopterPetTypePreference
 {
-    public class AdopterPetTypePreference :
-    {
-        [Required]
-        [Column("adopter_id", TypeName = "UUID")]
-        [ForeignKey(nameof(Adopter))]
-        public Guid AdopterId { get; set; }
-        public Adopter Adopter { get; set; } = null!;
+    [Required]
+    [Column("adopter_id", TypeName = "UUID")]
+    [ForeignKey(nameof(Adopter))]
+    public Guid AdopterId { get; set; }
+    public Adopter Adopter { get; set; } = null!;
 
-        [Required]
-        [Column("type_id", TypeName = "UUID")]
-        [ForeignKey(nameof(PetType))]
-        public Guid PetTypeId { get; set; }
-        public PetType PetType { get; set; } = null!;
-    }
-
+    [Required]
+    [Column("type_id", TypeName = "UUID")]
+    [ForeignKey(nameof(PetType))]
+    public Guid PetTypeId { get; set; }
+    public PetType PetType { get; set; } = null!;
 }
