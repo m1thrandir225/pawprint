1abf1e912378151a74242cc3599ab5e4ec6169e4
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-19 19:28:32 +0100

    feat: added column definitions to Vaccination class

--- Domain/Vaccination.cs
+++ Domain/Vaccination.cs
@@ -6,12 +6,17 @@ namespace Domain;
 [Table("vaccinations")]
 public class Vaccination : BaseEntity
 {
-    // I think this annotation isn't required.
-    [ForeignKey(nameof(MedicalRecord))] public Guid MedicalRecordId { get; set; }
+    [Column("medical_record_id", TypeName = "UUID")]
+    [ForeignKey(nameof(MedicalRecord))]
+    public Guid MedicalRecordId { get; set; }
 
-    [Required] public string VaccineName { get; set; }
+    [Required]
+    [Column("vaccine_name", TypeName = "TEXT")]
+    public string VaccineName { get; set; }
 
-    [Required] public DateOnly VaccineDate { get; set; }
+    [Required]
+    [Column("vaccine_date", TypeName = "Date")]
+    public DateOnly VaccineDate { get; set; }
 
     public virtual MedicalRecord MedicalRecord { get; set; }
 }
