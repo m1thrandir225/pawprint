96aa0ae1a2caccdc58197a6073e8d42969840d33
Sara Apostolovska <sara.apostolovska@students.finki.ukim.mk>
2024-12-19 12:43:44 +0100

    Adjusted according to new DB.

--- Domain/OwnerPetListing.cs
+++ Domain/OwnerPetListing.cs
@@ -11,22 +11,10 @@ public class OwnerPetListing : BaseEntity
     public Adopter Adopter { get; set; } = null!;
     
     [Required]
-    [Column("type_id", TypeName = "UUID")]
-    [ForeignKey(nameof(PetType))]
-    public Guid PetTypeId { get; set; }
-    public PetType PetType { get; set; } = null!;
-    
-    [Required]
-    [Column("gender_id", TypeName = "UUID")]
-    [ForeignKey(nameof(PetGender))]
-    public Guid PetGenderId { get; set; }
-    public PetGender PetGender { get; set; } = null!;
-    
-    [Required]
-    [Column("size_id", TypeName = "UUID")]
-    [ForeignKey(nameof(PetSize))]
-    public Guid PetSizeId { get; set; }
-    public PetSize PetSize { get; set; } = null!;
+    [Column("pet_id", TypeName = "UUID")]
+    [ForeignKey(nameof(Pet))]
+    public Guid PetId { get; set; }
+    public Pet Pet { get; set; } = null!;
     
     [Required]
     [Column("surrender_reason_id", TypeName = "UUID")]
@@ -34,11 +22,9 @@ public class OwnerPetListing : BaseEntity
     public Guid SurrenderReasonId { get; set; }
     public SurrenderReason SurrenderReason { get; set; } = null!;
     
-    [Required]
-    [Column("created_pet_id", TypeName = "UUID")]
-    [ForeignKey(nameof(Pet))]
-    public Guid PetId { get; set; }
-    public Pet Pet { get; set; } = null!;
-    
-    public ICollection<OwnerPetListingDocumets> OwnerPetListingDocuments { get; set; } = null!;
+    [Column("review_date", TypeName = "TIMESTAMPTZ")]
+    public DateTime ReviewDate { get; set; } = DateTime.Now;
+    [Column("submission_date", TypeName = "TIMESTAMPTZ")]
+    public DateTime SubmissionDate { get; set; } = DateTime.Now;
+    public virtual ICollection<OwnerPetListingDocumets> OwnerPetListingDocuments { get; set; } = null!;
 }
