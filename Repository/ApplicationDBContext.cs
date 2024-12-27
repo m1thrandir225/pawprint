6cdc1d3da1694bb92f63d8d195657eae68754b50
mare <markoilioski21@gmail.com>
2024-12-27 14:43:55 +0100

    Removed old comments.

--- Repository/ApplicationDBContext.cs
+++ Repository/ApplicationDBContext.cs
@@ -7,21 +7,6 @@ namespace Repository;
 
 public class ApplicationDbContext : DbContext
 {
-    /*
-     * Tables
-     */
-
-    // public DbSet<HealthStatus> HealthStatuses { get; set; }
-    // public DbSet<AdoptionStatus> AdoptionStatuses { get; set; }
-    //
-    // public DbSet<AdopterPetTypePreference> AdopterPetTypePreferences { get; set; }
-    // public DbSet<AdopterPetGenderPreference> AdopterPetGenderPreferences { get; set; }
-    // public DbSet<AdopterPetSizePreference> AdopterPetSizePreferences { get; set; }
-    //
-    // public DbSet<User> Users { get; set; }
-    // public DbSet<ShelterPetListing> ShelterPetListings { get; set; }
-    // public DbSet<OwnerPetListing> OwnerPetListings { get; set; }
-    
     public DbSet<AdopterPetGenderPreference> AdopterPetGenderPreferences { get; set; }
     public DbSet<AdopterPetSizePreference> AdopterPetSizePreferences { get; set; }
     public DbSet<AdopterPetTypePreference> AdopterPetTypePreferences { get; set; }
