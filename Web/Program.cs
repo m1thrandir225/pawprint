b008ab6e0dcfe6672d9740d27aa11087965011b1
mare <markoilioski21@gmail.com>
2024-12-29 22:43:42 +0100

    Made a test rest controller for Pet.cs

--- Web/Program.cs
+++ Web/Program.cs
@@ -19,6 +19,15 @@ builder.Services.AddScoped<IAdopterPetGenderPreferenceRepository, AdopterPetGend
 builder.Services.AddScoped<IPetTypeRepository, PetTypeRepository>();
 builder.Services.AddScoped<IPetRepository, PetRepository>();
 
+// Controllers 
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
 
 var app = builder.Build();
 
