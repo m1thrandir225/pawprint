d1f2d1d02479391e619e3869f1c1fcc138fe874f
Sara Apostolovska <sara.apostolovska@students.finki.ukim.mk>
2025-01-03 18:25:45 +0100

    Added AdopterPetSizePreference and AdopterPetTypePreference Repositories in Program.cs, AdopterPetGender alredy there.

--- Web/Program.cs
+++ Web/Program.cs
@@ -34,6 +34,8 @@ builder.Services.AddScoped<IShelterRepository, ShelterRepository>();
 builder.Services.AddScoped<IVeterinarianSpecilizationRepository, VeterinarianSpecilizationRepository>();
 builder.Services.AddScoped<IMedicalConditionRepository, MedicalConditionRepository>();
 builder.Services.AddScoped<IPetRepository, PetRepository>();
+builder.Services.AddScoped<IAdopterPetSizePreferenceRepository, AdopterPetSizePreferenceRepository>()
+builder.Services.AddScoped<IAdopterPetTypePreferenceRepository, AdopterPetTypePreferenceRepository>();
 // Controllers 
 
 builder.Services.AddControllers()
