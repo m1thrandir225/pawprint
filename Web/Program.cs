8d69b140d997f713c405a96622a74da6af6d090c
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-02-06 21:12:06 +0100

    feat: add redirect to frontend when accessing backend

--- Web/Program.cs
+++ Web/Program.cs
@@ -213,7 +213,7 @@ app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}");
 
 using (var scope = app.Services.CreateScope())
 {
